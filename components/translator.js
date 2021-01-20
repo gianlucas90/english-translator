34aa0899990b6105f1a5323046c3b53c69318f3a
Gianluca <gianlucazin1990@gmail.com>
2021-01-20 16:09:21 +0100

    project done

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,108 @@
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require('./american-to-british-titles.js');
+const britishOnly = require('./british-only.js');
 
 class Translator {
+  americanToBritish(string) {
+    let translation = string;
 
+    for (const word in americanToBritishSpelling) {
+      const regex = new RegExp(`\\b${word}\\b`, 'i');
+      // console.log(regex);
+      if (regex.test(string)) {
+        // console.log('object');
+        translation = string.replace(
+          regex,
+          this.getSpan(americanToBritishSpelling[word])
+        );
+      }
+    }
+    // console.log(translation);
+
+    for (const word in americanOnly) {
+      const regex = new RegExp(`\\b${word}\\b`, 'i');
+      if (regex.test(string)) {
+        translation = string.replace(regex, this.getSpan(americanOnly[word]));
+      }
+    }
+    // console.log(translation);
+
+    for (const word in americanToBritishTitles) {
+      const regex = new RegExp(word, 'i');
+      if (regex.test(string)) {
+        // Replacement
+        let repl = americanToBritishTitles[word];
+        // Capitalize replacement
+        let capRepl = repl.charAt(0).toUpperCase() + repl.slice(1);
+        // Replace in string
+        translation = string.replace(regex, this.getSpan(capRepl));
+      }
+    }
+
+    // Match Time
+    const timeRegex = new RegExp('\\d{1,2}:\\d{1,2}', 'ig');
+    const match = translation.match(timeRegex);
+    if (match) {
+      let changedformat = match.join('').replace(':', '.');
+      translation = translation.replace(timeRegex, this.getSpan(changedformat));
+    }
+
+    return translation;
+  }
+
+  britishToAmerican(string) {
+    let translation = string;
+
+    for (const word in americanToBritishSpelling) {
+      const regex = new RegExp(`\\b${americanToBritishSpelling[word]}\\b`, 'i');
+      if (regex.test(string)) {
+        translation = translation.replace(regex, this.getSpan(word));
+      }
+    }
+    // console.log(translation);
+
+    for (const word in britishOnly) {
+      const regex = new RegExp(`\\b${word}\\b`, 'i');
+      if (regex.test(string)) {
+        translation = translation.replace(
+          regex,
+          this.getSpan(britishOnly[word])
+        );
+      }
+    }
+    // console.log(translation);
+
+    for (const word in americanToBritishTitles) {
+      const regex = new RegExp(`\\b${americanToBritishTitles[word]}\\b`, 'i');
+      if (regex.test(string)) {
+        // Replacement
+        let repl = word;
+        // Capitalize replacement
+        let capRepl = repl.charAt(0).toUpperCase() + repl.slice(1);
+        // Replace in string
+        translation = translation.replace(regex, this.getSpan(capRepl));
+      }
+    }
+
+    // Match Time
+    const timeRegex = new RegExp('\\d{1,2}.\\d{1,2}', 'ig');
+    const match = translation.match(timeRegex);
+    if (match) {
+      let changedformat = match.join('').replace('.', ':');
+      translation = translation.replace(timeRegex, this.getSpan(changedformat));
+    }
+
+    return translation;
+  }
+
+  getSpan(word) {
+    return `<span class="highlight">${word}</span>`;
+  }
+
+  stripHtml(originalString) {
+    return originalString.replace(/(<([^>]+)>)/gi, '');
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,29 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route('/api/translate').post((req, res) => {
+    const { text, locale } = req.body;
+    let translation;
+
+    if (text === '') return res.json({ error: 'No text to translate' });
+
+    if (!text || !locale)
+      return res.json({ error: 'Required field(s) missing' });
+
+    if (locale !== 'american-to-british' && locale !== 'british-to-american')
+      return res.json({ error: 'Invalid value for locale field' });
+
+    if (locale === 'american-to-british')
+      translation = translator.americanToBritish(text);
+
+    if (locale === 'british-to-american')
+      translation = translator.britishToAmerican(text);
+
+    if (translation === text)
+      return res.json({ text, translation: 'Everything looks good to me!' });
+
+    res.json({ text, translation });
+  });
 };
